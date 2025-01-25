4d856a8ad9ac9060f70bbe1b4b5bd7571c74e0fd
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-01-25 16:45:43 +0000

    1

---
+++ buildall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export MAVEN_OPTS=-Xmx2048m
+cd /c/workspaces/open-source-projects/unified-namespace/utilities
+mvn -s C:/Users/neo/.m2/settingsOrion.xml clean install
+cd /c/workspaces/open-source-projects/unified-namespace/api
+mvn -s C:/Users/neo/.m2/settingsOrion.xml clean install
+cd /c/workspaces/open-source-projects/unified-namespace/uns-app-backend
+mvn -s C:/Users/neo/.m2/settingsOrion.xml clean install
---
+++ startfrontend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /c/workspaces/open-source-projects/unified-namespace/uns-app-frontend/app
+http-server -p 8081
